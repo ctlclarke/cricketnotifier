b067e6edf3ae9e77a47591c08ed657f299843009
Laptop Ed <boblettoj99@gmail.com>
2020-01-25 13:24:51 +0000

    fixed async request

--- cricinfo.js
+++ cricinfo.js
@@ -4,7 +4,7 @@ const https = require('https');
 const getRecentBall = async () => {
 
 
-    https.get("https://site.web.api.espn.com/apis/site/v2/sports/cricket/19430/playbyplay?contentorigin=espn&event=1185307&period=1&section=cricinfo", (resp) => {
+    return new Promise((resolve, reject) => { https.get("https://site.web.api.espn.com/apis/site/v2/sports/cricket/19430/playbyplay?contentorigin=espn&event=1185307&section=cricinfo&period=2", (resp) => {
         let data = '';
       
         // A chunk of data has been recieved.
@@ -15,45 +15,49 @@ const getRecentBall = async () => {
         // The whole response has been received. Print out the result.
         resp.on('end', () => {
           var numberOfPages = JSON.parse(data).commentary.pageCount;
+          console.log(numberOfPages);
       
-      
-          https.get("https://site.web.api.espn.com/apis/site/v2/sports/cricket/19430/playbyplay?contentorigin=espn&event=1185307&page=" + numberOfPages + "&period=1&section=cricinfo", (resp) => {
-              data = '';
-              resp.on('data', (chunk) => {
-                  data += chunk;
-                });
-              resp.on('end', () => {
-                  var items = JSON.parse(data).commentary.items;
-      
-                  var maxId = 0
-                  var newestText = ""
-                  var recentScore = ""
-                  var overs = ""
-      
-                  items.forEach((item) => {
-                      if (item.id > maxId)
-                      {
-                          maxId = item.id
-                          newestText = item.shortText
+          https.get("https://site.web.api.espn.com/apis/site/v2/sports/cricket/19430/playbyplay?contentorigin=espn&event=1185307&page=" + numberOfPages + "&section=cricinfo&period=2", (resp) => {
+            data = '';
+            resp.on('data', (chunk) => {
+                data += chunk;
+              });
+            resp.on('end', () => {
+                var items = JSON.parse(data).commentary.items;
+    
+                var maxId = 0
+                var newestText = ""
+                var recentScore = ""
+                var overs = ""
+    
+                items.forEach((item) => {
+                    if (item.id > maxId)
+                    {
+                        maxId = item.id
+                        newestText = item.shortText
+                        console.log(item.period + ' ' + item.homeScore + ' ' + item.awayScore);
+                        if (item.period % 2 == 1)
+                        {
                           recentScore = item.homeScore
-                          overs = item.over.overs
-      
-                      }
-                  })
-      
-      
-                  return { overs, recentScore, newestText }
-      
-      
-      
-              })
-          })
-      
+                        }
+                        else
+                        {
+                          recentScore = item.awayScore
+                        }
+                        overs = item.over.overs
+    
+                    }
+                })
+                resolve({ overs, recentScore, newestText });
+            });
+          });      
         });
       
       }).on("error", (err) => {
         console.log("Error: " + err.message);
+        reject(err);
       });
+    });
 
 
 
--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const notifier = require('node-notifier');
+const notificationService = require('./notification-service');
 const path = require('path');
+const cricinfo = require('./cricinfo')
 
-notifier.notify(
-  {
-    title: 'My awesome title',
-    message: 'Hello from node, Mr. User!',
-    icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
-    sound: true, // Only Notification Center or Windows Toasters
-    wait: true // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait or notify-send as it does not support the wait option
-  },
-  function(err, response) {
-    // Response is response from notification
-    console.log("DONE")
-  }
-);
+async function demo() {
 
-notifier.on('click', function(notifierObject, options, event) {
-  // Triggers if `wait: true` and user clicks notification
-});
+  const message = await cricinfo.getRecentBall();
+  console.log(message);
 
-notifier.on('timeout', function(notifierObject, options) {
-  // Triggers if `wait: true` and notification closes
-});
+  notificationService.notify(
+    {
+      title: message.overs + ' - ' + message.recentScore,
+      message: message.newestText,
+      icon: path.join(__dirname, 'coulson.jpg'), // Absolute path (doesn't work on balloons)
+      sound: true, // Only Notification Center or Windows Toasters
+      wait: true // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait or notify-send as it does not support the wait option
+    },
+    function(err, response) {
+      // Response is response from notification
+      console.log("DONE")
+    }
+  );
+}
+
+demo();
