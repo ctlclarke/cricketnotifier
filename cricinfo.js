951ea9e483bbb153f5e04b0a8d01a5fdf2371982
Edmund Bevan <ebevan@caci.co.uk>
2020-01-27 09:03:49 +0000

    Fixed do while issue

--- cricinfo.js
+++ cricinfo.js
@@ -1,158 +1,150 @@
-const https = require('https');
+const https = require("https");
 let apiUrl = "";
 
-const getScorecardForPeriod = async (period) => {
-    return new Promise((resolve, reject) => { https.get(apiUrl + "&period=" + period, (resp) => {
-        let data = '';
-      
+const getScorecardForPeriod = async period => {
+  return new Promise((resolve, reject) => {
+    https
+      .get(apiUrl + "&period=" + period, resp => {
+        let data = "";
+
         // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
+        resp.on("data", chunk => {
           data += chunk;
         });
-      
+
         // The whole response has been received. Print out the result.
-        resp.on('end', () => {
+        resp.on("end", () => {
           var d = JSON.parse(data).commentary;
-           resolve(d.items.length != 0);
-
-              
+          resolve(d.items.length != 0);
         });
-      
-      }).on("error", (err) => {
+      })
+      .on("error", err => {
         console.log("Error: " + err.message);
         reject(err);
       });
-    });
-}
+  });
+};
 
-const getNumberOfPages = async (period) => {
-    return new Promise((resolve, reject) => { https.get(apiUrl + "&period=" + period, (resp) => {
+const getNumberOfPages = async period => {
+  return new Promise((resolve, reject) => {
+    https
+      .get(apiUrl + "&period=" + period, resp => {
+        let data = "";
 
-        let data = '';
-      
         // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
+        resp.on("data", chunk => {
           data += chunk;
         });
-      
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            var d = JSON.parse(data).commentary;
-            var numberOfPages = d.pageCount;
-            resolve(numberOfPages);
 
-              
+        // The whole response has been received. Print out the result.
+        resp.on("end", () => {
+          var d = JSON.parse(data).commentary;
+          var numberOfPages = d.pageCount;
+          resolve(numberOfPages);
         });
-      
-      }).on("error", (err) => {
+      })
+      .on("error", err => {
         console.log("Error: " + err.message);
         reject(err);
       });
-    });
-}
-
+  });
+};
 
 const getInitial = async () => {
+  var potentialPeriod = 4;
 
-    var potentialPeriod = 4
+  var haveHadPeriodYet = await getScorecardForPeriod(potentialPeriod);
+  while (haveHadPeriodYet == false) {
+    potentialPeriod -= 1;
+    haveHadPeriodYet = await getScorecardForPeriod(potentialPeriod);
+  }
+  var currentPeriod = potentialPeriod;
 
-    var haveHadPeriodYet = await getScorecardForPeriod(potentialPeriod)
-    do {
-        potentialPeriod -= 1
-        haveHadPeriodYet = await getScorecardForPeriod(potentialPeriod)
-    } while (haveHadPeriodYet == false)
-    var currentPeriod = potentialPeriod
-    
-    var numberOfPages = await getNumberOfPages(currentPeriod)
+  var numberOfPages = await getNumberOfPages(currentPeriod);
 
-    return { currentPeriod, numberOfPages }
-    // return period and number of pages
-}
-
-
+  return { currentPeriod, numberOfPages };
+  // return period and number of pages
+};
 
 const getRecentBall = async () => {
+  var i = await getInitial();
+  var currentPeriod = i.currentPeriod;
+  var numberOfPages = i.numberOfPages;
 
+  return new Promise((resolve, reject) => {
+    https
+      .get(
+        apiUrl + "&page=" + numberOfPages + "&period=" + currentPeriod,
+        resp => {
+          data = "";
+          resp.on("data", chunk => {
+            data += chunk;
+          });
+          resp.on("end", () => {
+            var items = JSON.parse(data).commentary.items;
 
-    var i = await getInitial()
-    var currentPeriod = i.currentPeriod
-    var numberOfPages = i.numberOfPages
+            var maxId = 0;
+            var newestText = "";
+            var recentScore = "";
+            var overs = "";
+            var id = "";
 
-    return new Promise((resolve, reject) => { https.get(apiUrl + "&page=" + numberOfPages + "&period=" + currentPeriod, (resp) => {
-
-
-            data = '';
-            resp.on('data', (chunk) => {
-                data += chunk;
-              });
-            resp.on('end', () => {
-                var items = JSON.parse(data).commentary.items;
-    
-                var maxId = 0
-                var newestText = ""
-                var recentScore = ""
-                var overs = ""
-                var id = ""
-    
-                items.forEach((item) => {
-                    if (item.sequence > maxId)
-                    {
-                        maxId = item.sequence
-                        newestText = item.shortText
-                        if (item.period % 2 == 1)
-                        {
-                          recentScore = item.homeScore
-                        }
-                        else
-                        {
-                          recentScore = item.awayScore
-                        }
-                        overs = item.over.overs
-                        id = item.id
-    
-                    }
-                })
-                resolve({ overs, recentScore, newestText, id });
-            });    
-
-      
-      }).on("error", (err) => {
+            items.forEach(item => {
+              if (item.sequence > maxId) {
+                maxId = item.sequence;
+                newestText = item.shortText;
+                if (item.period % 2 == 1) {
+                  recentScore = item.homeScore;
+                } else {
+                  recentScore = item.awayScore;
+                }
+                overs = item.over.overs;
+                id = item.id;
+              }
+            });
+            resolve({ overs, recentScore, newestText, id });
+          });
+        }
+      )
+      .on("error", err => {
         console.log("Error: " + err.message);
         reject(err);
       });
-    });
+  });
+};
 
-
-
-}
-
-const setupUrl = (eventUrl) => {
+const setupUrl = eventUrl => {
   const urlParts = eventUrl.split("/");
   let series = null;
   let event = null;
   let seriesNext = false;
   let eventNext = false;
   urlParts.forEach(part => {
-    if(seriesNext) {
+    if (seriesNext) {
       series = part;
       seriesNext = false;
     }
-    if(eventNext) {
+    if (eventNext) {
       event = part;
       eventNext = false;
     }
-    if(part === 'series') {
+    if (part === "series") {
       seriesNext = true;
     }
-    if(part === 'game') {
+    if (part === "game") {
       eventNext = true;
     }
   });
-  if(event == null || series == null) {
-    throw 'Bad URL';
+  if (event == null || series == null) {
+    throw "Bad URL";
   }
-  apiUrl = "https://site.web.api.espn.com/apis/site/v2/sports/cricket/" + series + "/playbyplay?contentorigin=espn&event=" + event + "&section=cricinfo";
-}
+  apiUrl =
+    "https://site.web.api.espn.com/apis/site/v2/sports/cricket/" +
+    series +
+    "/playbyplay?contentorigin=espn&event=" +
+    event +
+    "&section=cricinfo";
+};
 
 exports.setupUrl = setupUrl;
 exports.getRecentBall = getRecentBall;
